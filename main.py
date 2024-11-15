7c0ece9ac2267351392b4293a1cb6186759bd98f
layz <broitzlayz@gmail.com>
2024-11-15 22:00:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import aiohttp
+import asyncio
+
+import uuid
+
+from tqdm.asyncio import tqdm
+
+URL = "https://ngl.link/api/submit"
+
+try:
+    with open("proxies.txt") as file:
+        proxies = set(line.strip() for line in file.readlines())
+except FileNotFoundError:
+    proxies = set()
+
+
+def get_proxy():
+    if not proxies:
+        return None
+    return proxies.pop()
+
+
+async def send_questions(username: str, question: str, amount: int):
+    async with aiohttp.ClientSession() as session:
+        for _ in tqdm(range(amount), desc="Sending questions"):
+            proxy = get_proxy()
+            if proxy:
+                proxy_url = f"http://{proxy}"
+            else:
+                proxy_url = None
+
+            try:
+                async with session.post(
+                    URL,
+                    data={
+                        "username": username,
+                        "question": question,
+                        "deviceId": str(uuid.uuid4()),
+                        "gameSlug": "",
+                        "referrer": "https://l.instagram.com/",
+                    },
+                    proxy=proxy_url,
+                ) as response:
+                    try:
+                        await response.json()
+                    except Exception:
+                        await asyncio.sleep(1)
+
+            except aiohttp.ClientError:
+                print(f"Bad request ({proxy})")
+
+
+async def main():
+    print(
+        r"""
+  _   _  _____ _         _____ _____        __  __ __  __ ______ _____  
+ | \ | |/ ____| |       / ____|  __ \ /\   |  \/  |  \/  |  ____|  __ \ 
+ |  \| | |  __| |      | (___ | |__) /  \  | \  / | \  / | |__  | |__) |
+ | . ` | | |_ | |       \___ \|  ___/ /\ \ | |\/| | |\/| |  __| |  _  / 
+ | |\  | |__| | |____   ____) | |  / ____ \| |  | | |  | | |____| | \ \ 
+ |_| \_|\_____|______| |_____/|_| /_/    \_\_|  |_|_|  |_|______|_|  \_\
+                                                                        
+        """
+    )
+
+    username = input("Enter username: ")
+    question = input("Enter question: ")
+
+    while True:
+        try:
+            amount = int(input("Enter amount of questions: "))
+            if amount <= 0:
+                raise ValueError("Amount must be higher than 0")
+            break
+        except ValueError as e:
+            print(e)
+
+    await send_questions(username, question, amount)
+    print("Done!")
+
+
+asyncio.run(main())
